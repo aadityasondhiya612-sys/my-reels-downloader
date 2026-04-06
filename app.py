2ea2de7b00fb68f7ab636705da6b9a0a200ce302
aadityasondhiya612-sys <aadityasondhiya612@gmail.com>
2026-04-06 12:49:25 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    video_url = request.form.get('url')
+    try:
+        ydl_opts = {'format': 'best', 'quiet': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(video_url, download=False)
+            video_direct_url = info.get('url')
+        return render_template('index.html', download_link=video_direct_url)
+    except:
+        return "Invalid Link! Dubara koshish karein."
+
+if __name__ == "__main__":
+    app.run()
